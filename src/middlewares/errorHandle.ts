576c48a9ac5dd7e7ccc0d48bc7e786b2a7b72748
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-08-30 11:01:04 -0300

    Update errorHandle.ts

--- src/middlewares/errorHandle.ts
+++ src/middlewares/errorHandle.ts
@@ -3,9 +3,12 @@ import InvalidData from '../errors/InvalidData';
 import DoubleReport from '../errors/DoubleReport';
 import ConfirmationDuplicate from '../errors/ConfirmationDuplicate';
 
-const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+function isValidationError(err: Error) {
+    return [InvalidData, DoubleReport, ConfirmationDuplicate].some(errorClass => err instanceof errorClass);
+}
 
-    if (err instanceof InvalidData || DoubleReport || ConfirmationDuplicate) {
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (isValidationError(err)) {
         res.status(err.statusCode).json({
             error_code: err.errorCode,
             error_description: err.message
